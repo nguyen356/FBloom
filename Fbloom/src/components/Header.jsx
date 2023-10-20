f58daa3a8983fdcb0b2d57c6b3e0a0c8070cc9d7
nguyen356 <showyellow1@yahoo.com>
2023-10-21 02:11:33 +0700

    update some folder

---
+++ Fbloom/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import {FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className='bg-blue-200'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-5'>
+            <Link to='/'>
+                {/* <span><img alt="Snowy mountain lake" class="object-cover h-full min-w-full" src="FBloom/src/assets/logo.png"/></span> */}
+        <h1 className='font-serif text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-pink-500'>Flower</span>
+            <span className='text-emerald-300'>Bloom</span>
+        </h1>
+        </Link>
+        <ul className='flex gap-4'/>
+        <Link to='/'><li className='hidden sm:inline text-slate-600 hover:underline'>Home</li></Link>
+        <Link to='about'><li className='hidden sm:inline text-slate-600 hover:underline'>About</li></Link>
+        <Link to='sign-up'><li className='hidden sm:inline text-slate-600 hover:underline'>Sign Up</li></Link>
+        <Link to='sign-in'><li className=' text-slate-700 hover:underline'>Sign In</li></Link>
+        <form className='bg-slate-100 p-1 rounded-lg flex items-center'>
+            <input type="text" placeholder="Search" className='bg-transparent outline-none w-24 sm:w-50'/>
+            <FaSearch className='text-slate-500'/>
+        </form>
+        </div>
+    </header>
+  )
+}
