86fd6471fb608e4f10a61bbdde8844a8af7ff5f7
nguyen356 <showyellow1@yahoo.com>
2023-10-27 10:34:03 +0700

    api update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() =>{
@@ -8,8 +10,14 @@ console.log('Connect to DB!');
 }).catch((err) =>{
     console.log(err);
 })
+
 const app = express();
 
-app.listen(5000, () =>{
-    console.log('server is running on port 6789!!!');
+app.use(express.json());
+
+app.listen(3000, () =>{
+    console.log('server is running on port 3000!!!');
 });
+
+app.use("api/user", userRouter);
+app.use("api/auth", authRouter);
---
+++ api/routes/user.route.js
@@ -0,0 +1,5 @@
+import express from 'express';
+import { testing } from '../controllers/user.controller.js';
+const router = express.Router();
+router.get('/testing', testing);
+export default router;
