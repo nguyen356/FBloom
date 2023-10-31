5793a6528b4c5a4e03e8f3a63c00670031102ef0
nguyen356 <showyellow1@yahoo.com>
2023-10-31 10:31:44 +0700

    install -g to fix problem

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,15 @@
 import User from '../models/user.models.js';
+import bcyptjs from 'bcryptjs';
 
 export const signup = async (req, res) => {
 const {username, email,password} = req.body;
-const newUser = new User({username, email, password});
-await newUser.save()
+const hashPassword = bcyptjs.hashSync(password, 10);
+const newUser = new User({username, email, password: hashPassword});
+try {
+    await newUser.save()
 res.status(201).json("User created succesfully");
+} catch (error) {
+    res.status(500).json(error.message);
+}
+
 };
