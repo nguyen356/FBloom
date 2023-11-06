d86d5fb2bd95565856bcbaedfb3d2c982d8ebf59
nguyen356 <showyellow1@yahoo.com>
2023-11-07 00:42:41 +0700

    update avatar user

--- Sanctuary/src/components/Header.jsx
+++ Sanctuary/src/components/Header.jsx
@@ -1,25 +1,36 @@
 import {FaSearch} from 'react-icons/fa';
 import {Link} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 export default function Header() {
+  const {currentUser} = useSelector(state => state.user)
   return (
     <header className='bg-blue-200'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-5'>
             <Link to='/'>
         <h1 className='font-serif text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-pink-500'>Flower</span>
-            <span className='text-emerald-300'>Bloom</span>
+            <span className='text-black'>Sanctuary </span>
+            <span className='text-black'>Apartment </span>
         </h1>
         </Link>
         <ul className='flex gap-4'/>
         <Link to='/'><li className='hidden sm:inline text-slate-600 hover:underline'>Home</li></Link>
+
         <Link to='about'><li className='hidden sm:inline text-slate-600 hover:underline'>About</li></Link>
+
         <Link to='sign-up'><li className='hidden sm:inline text-slate-600 hover:underline'>Sign Up</li></Link>
-        <Link to='sign-in'><li className='hidden sm:inline text-slate-700 hover:underline'>Sign In</li></Link>
+
+        <Link to='/profile'>
+          {currentUser ? (<img className='rounded-full h-10 w-10 object-cover' src={currentUser.imageavatar} alt='profile'/>): (
+          <li className='hidden sm:inline text-slate-700 hover:underline'>Sign In</li> )}
+          </Link> 
+          
+
         <form className='bg-slate-100 p-1 rounded-lg flex items-center'>
             <input type="text" placeholder="Search" className='bg-transparent outline-none w-24 sm:w-50'/>
             <FaSearch className='text-slate-500'/>
         </form>
+
         </div>
     </header>
   )
