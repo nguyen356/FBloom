caa4db18d78b6e957557a8ed3615ad4888197789
nguyen356 <showyellow1@yahoo.com>
2023-11-26 22:35:02 +1030

    delete user function

--- Sanctuary/src/pages/Profile.jsx
+++ Sanctuary/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import {useRef, useState, useEffect} from 'react';
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart,updateUserSuccess,updateUserFailure } from '../redux/user/userSlice';
+import { updateUserStart,updateUserSuccess,updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess } from '../redux/user/userSlice';
 
 export default function Profile() {
   const fileRef = useRef(null)
@@ -71,6 +71,23 @@ export default function Profile() {
     }
   };
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`,{
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-mono mt-7'>Profile</h1>
@@ -92,7 +109,7 @@ export default function Profile() {
         <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-80 disabled:opacity-80'>{loading ? 'loading...': 'Update'}</button>
       </form>
       <div className='flex justify-between mt-2'>
-        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -30,3 +30,14 @@ res.status(200).json(rest);
         next(error)
     }
 };
+
+export const deleteUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id) return next (errorHandler(401, 'you can only delete your own account! '));
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.clearCookie('access_token');
+        res.status(200).json('User have been delete!!!');
+    } catch (error) {
+        next(error)
+    }
+}
