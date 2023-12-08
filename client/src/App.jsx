4a5b65d36e2cf8856b612edb20ce1a984019182a
nguyen356 <showyellow1@yahoo.com>
2023-12-09 03:44:07 +1030

    fixing error term 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
-//import React from 'react'//
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
-import Createlisting from './pages/Createlisting';
-import Updatelisting from './pages/UpdateListing';
 import PrivateRoute from './components/PrivateRoute';
+import Createlisting from './pages/CreateListing';
+import Updatelisting from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
 
