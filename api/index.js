7292f2c25c36642268651625881dda54ef0b70c6
nguyen356 <showyellow1@yahoo.com>
2023-12-09 01:42:15 +1030

    prepare to deploy

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() =>{
@@ -13,6 +14,7 @@ console.log('Connect to Database!');
     console.log(err);
 })
 
+const __dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -31,6 +33,11 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+app.use(express.static(path.join(__dirname, '/sanctuary/dist')));
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'sanctuary', 'dist', 'index.html'));
+})
 
 
 app.use((err, req, res, next) => {
